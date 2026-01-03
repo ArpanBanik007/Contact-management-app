55fcbe6293f803a60639bc9de520d37ec51fb2fc
Arpan Banik <arpanbanik976@gmail.com>
2026-01-03 16:19:14 +0530

    Updated Dist file

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-
+import path from path
 
 const app= express();
 
@@ -24,5 +24,28 @@ app.use(express.static("public"))// Access for public file data to server
 import contactRoutes from "./router/contact.router.js"
 
 app.use("/api/v1/contact", contactRoutes); 
+
+
+
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+
+  app.use(
+    express.static(
+      path.join(__dirname, "Contact-Frontend", "dist")
+    )
+  );
+
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.join(
+        __dirname,
+        "Contact-Frontend",
+        "dist",
+        "index.html"
+      )
+    );
+  });
+}
 
 export default app
