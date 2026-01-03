d3fc244ae06cc78c27eca4dee437d1db29b97eec
Arpan Banik <arpanbanik976@gmail.com>
2026-01-03 17:28:27 +0530

    App.js

--- app.js
+++ app.js
@@ -1,45 +1,34 @@
 import express from "express";
 import cors from "cors";
-import path from "path"
+import path from "path";
 
-const app= express();
+const app = express();
 
-
-
-// cors frontend
-
+// CORS frontend
 app.use(cors({
   origin: ["http://localhost:5173", "http://localhost:5174"],
   credentials: true,
 }));
 
+app.use(express.json({ limit: "10kb" }));
+app.use(express.urlencoded({ extended: true, limit: "10kb" })); // For nested object
+app.use(express.static("public")); // Access public files
 
+import contactRoutes from "./router/contact.router.js";
 
-app.use(express.json({ limit: "10kb" }))
-app.use(express.urlencoded({ extended: true, limit: "10kb" }))// For nested object
-app.use(express.static("public"))// Access for public file data to server
+app.use("/api/v1/contact", contactRoutes);
 
-
-
-import contactRoutes from "./router/contact.router.js"
-
-app.use("/api/v1/contact", contactRoutes); 
-
-
-
+// Production frontend build
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
 
-  app.use(
-    express.static(
-      path.join(__dirname, "Contact-Frontend/dist")
-    )
-  );
+  // Serve static files
+  app.use(express.static(path.join(__dirname, "Contact-Frontend", "dist")));
 
-  
-  app.get("./*",(req,res)=>{
-    res.sendFile(path.resolve(dirPath,'Contact-Frontend/dist','index.html'))
-  })
+  // Catch all route
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "Contact-Frontend", "dist", "index.html"));
+  });
 }
 
-export default app
+export default app;
