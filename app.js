3ae4f67ffc400cf27be2e6e4861913d91a88a3c3
Arpan Banik <arpanbanik976@gmail.com>
2026-01-03 17:33:34 +0530

    App.js

--- app.js
+++ app.js
@@ -4,29 +4,27 @@ import path from "path";
 
 const app = express();
 
-// CORS frontend
+// Middleware
 app.use(cors({
   origin: ["http://localhost:5173", "http://localhost:5174"],
   credentials: true,
 }));
-
 app.use(express.json({ limit: "10kb" }));
-app.use(express.urlencoded({ extended: true, limit: "10kb" })); // For nested object
-app.use(express.static("public")); // Access public files
+app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+app.use(express.static("public"));
 
+// Routes
 import contactRoutes from "./router/contact.router.js";
-
 app.use("/api/v1/contact", contactRoutes);
 
-// Production frontend build
+// Production frontend
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
 
-  // Serve static files
   app.use(express.static(path.join(__dirname, "Contact-Frontend", "dist")));
 
-  // Catch all route
-  app.get("*", (req, res) => {
+  // Safe catch-all route
+  app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, "Contact-Frontend", "dist", "index.html"));
   });
 }
